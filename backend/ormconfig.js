b375c91c3a8f6fc567b15b7088187a1abf16a1ca
Ossi Lehtonen <ossi.j.lehtonen@helsinki.fi>
2022-03-17 13:09:27 +0200

    config

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config;
-const { DATABASE_URL } = process.env;
+const { DATABASE_URL, NODE_ENV } = process.env;
 
 module.exports = {
   type: 'postgres',
@@ -9,5 +9,8 @@ module.exports = {
   logging: false,
   entities: ['src/models/**/*.ts', 'src/models/**/*.js'],
   migrations: ['src/migration/**/*.ts', 'src/migration/**/*.js'],
-  subscribers: ['src/subscriber/**/*.ts', 'src/subscriber/**/*.js']
+  subscribers: ['src/subscriber/**/*.ts', 'src/subscriber/**/*.js'],
+  extra: {
+    ssl: NODE_ENV === "production"
+  }
 };
