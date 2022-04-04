71d96b1832cfaa69c149bf6630776db88bf17e57
lehtoneo <48444546+lehtoneo@users.noreply.github.com>
2022-04-04 17:35:38 +0300

    Update index.ts

--- backend/src/util/index.ts
+++ backend/src/util/index.ts
@@ -1,7 +1,7 @@
 import { Event } from '../models/Event';
 import { Request } from 'express';
 const getEventUrlFromEvent = (req: Request, event: Event) => {
-  const baseUrl = req.protocol + '://' + req.get('host') + '/#';
+  const baseUrl = 'https://' + req.get('host') + '/#';
   const path = `/teams/${event.teamId}/events/${event.id}`;
   const url = baseUrl + path;
 
