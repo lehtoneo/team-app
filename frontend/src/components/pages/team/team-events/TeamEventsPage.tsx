0bf6c5be0dd4c4f523091bdaf7a2d21ae661d46a
lehtoneo <ossi.j.lehtonen@helsinki.fi>
2023-02-09 16:06:40 +0200

    remove unused code

--- frontend/src/components/pages/team/team-events/TeamEventsPage.tsx
+++ frontend/src/components/pages/team/team-events/TeamEventsPage.tsx
@@ -3,15 +3,11 @@ import { Link, Navigate, Route, Routes, useParams } from 'react-router-dom';
 
 import SingleEventPage from './SingleEventPage';
 import useTeam from '../../../../hooks/useTeam';
-import Button from '../../../Button';
 import Header from '../../../Header';
 import LoadingPage from '../../LoadingPage';
 import RequireTeamAuthPage from '../RequireTeamAuthPage';
-import useEventConnection from '../../../../hooks/useEventConnection';
 import CalendarContainer from '../../../calendar/CalendarContainer';
 import FieldInfo from '../../../forms/components/FieldInfo';
-import useEventTypeConnection from '../../../../hooks/useEventTypeConnection';
-import LoadingIndicator from '../../../LoadingIndicator';
 import EventTypes from './components/EventTypes';
 
 interface TeamEventsMainPageContentProps {
@@ -23,19 +19,9 @@ const TeamEventsMainPageContent: React.FC<TeamEventsMainPageContentProps> = (
 ) => {
   const { teamAuth, team } = useTeam({ id: props.teamId });
 
-  const { events, error: eventFetchError } = useEventConnection({
-    paginationInput: { first: 10000 },
-    eventFilters: { teamId: props.teamId }
-  });
-
-  const eventTypeConnection = useEventTypeConnection({
-    filterEventTypes: { teamId: props.teamId }
-  });
-
   return (
     <div>
       <Header size={2}>Events</Header>
-      {eventFetchError && <p>Error fetching events</p>}
       <FieldInfo>Create events by pressing a date in the calendar</FieldInfo>
       <CalendarContainer
         editable={teamAuth.event.writeRights}
